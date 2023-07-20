14643c4cd36c7bebef6178d0b81018e8c9e480bb
Harry-zklcdc <1269158832@qq.com>
2023-07-20 11:40:13 +0800

    [Add] 🎉 Voice Input Support

--- frontend/public/js/bing/chat/core.js
+++ frontend/public/js/bing/chat/core.js
@@ -10,10 +10,10 @@
 }
 )(_w.onload, _w.si_PP);
 _w.rms.js(
-  { 'A:rms:answers:Shared:BingCore.Bundle': '/rp/b4RftLcpNuuBy86wP4OweSL7-T0.br.js' },
-  { 'A:rms:answers:Web:SydneyFSCHelper': '/rp/4HTa8qkEYVg_8T6ueICX_pKDe50.br.js' },
+  { 'A:rms:answers:Shared:BingCore.Bundle': '/rp/Pki1-YEXD6vos5MiDyyAeDq7sgs.br.js' },
+  { 'A:rms:answers:Web:SydneyFSCHelper': '/rp/IKBASmxOJ_ubDLFkwxNAcql6AGY.br.js' },
   { 'A:rms:answers:VisualSystem:ConversationScope': '/rp/YFRe970EMtFzujI9pBYZBGpdHEo.br.js' },
-  { 'A:rms:answers:CodexBundle:cib-bundle': '/rp/z9_SsI02ef2T3LbfYch7omlQckY.br.js' },
+  { 'A:rms:answers:CodexBundle:cib-bundle': '/rp/wy2Kbr72SvrxylJaHweOTCpPs14.br.js' },
   { 'A:rms:answers:SharedStaticAssets:speech-sdk': '/rp/6slp3E-BqFf904Cz6cCWPY1bh9E.br.js' },
-  { 'A:rms:answers:Web:SydneyFullScreenConv': '/rp/p3B0R3uyb4l5fPv4-1oZWzJzYJc.br.js' }
+  { 'A:rms:answers:Web:SydneyFullScreenConv': '/rp/qj7paUiWxhCdv2VxdV3OWsUIoZ8.br.js' },
 );
