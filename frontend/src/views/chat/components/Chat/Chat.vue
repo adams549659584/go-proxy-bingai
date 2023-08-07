9e31e2ddae34c116aee6d61a6b065cd8c16efd23
Harry-zklcdc <1269158832@qq.com>
2023-08-07 11:10:41 +0800

    [Fix] 🤔 invalid Domain

--- frontend/src/views/chat/components/Chat/Chat.vue
+++ frontend/src/views/chat/components/Chat/Chat.vue
@@ -93,6 +93,7 @@ const initChatService = () => {
     }
     chatStore.checkAllSydneyConfig();
   }
+  CIB.config.captcha.baseUrl = 'https://www.bing.com'
   CIB.config.bing.baseUrl = location.origin;
   CIB.config.bing.signIn.baseUrl = location.origin;
   CIB.config.answers.baseUrl = location.origin;
