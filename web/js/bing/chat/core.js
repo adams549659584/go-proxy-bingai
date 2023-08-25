922b8c47d2d5c6e77137f29b78c8da3de95be841
Harry-zklcdc <zklcdc@qq.com>
2023-08-25 11:53:08 +0000

    [Upgrade] 🚀 Upload latest webFile

--- web/js/bing/chat/core.js
+++ web/js/bing/chat/core.js
@@ -11,11 +11,11 @@
 )(_w.onload, _w.si_PP);
 _w.rms.js(
   { 'A:rms:answers:Shared:BingCore.Bundle': '/rp/Pki1-YEXD6vos5MiDyyAeDq7sgs.br.js' },
-  { 'A:rms:answers:Web:SydneyFSCHelper': '/rp/plViotfzKXF_pqqXx4NWn6aW_II.br.js' },
+  { 'A:rms:answers:Web:SydneyFSCHelper': '/rp/E4f2o5_6Vx0sVO4-33-9vnCtvGA.br.js' },
   { 'A:rms:answers:VisualSystem:ConversationScope': '/rp/YFRe970EMtFzujI9pBYZBGpdHEo.br.js' },
-  { 'A:rms:answers:CodexBundle:cib-bundle': '/rp/h_z8hgmoir53hvshQQoyF7vDrYc.br.js' },
+  { 'A:rms:answers:CodexBundle:cib-bundle': '/rp/-RvmaFKGjpn4DGSFAYKuNNeLfXM.br.js' },
   { 'A:rms:answers:SharedStaticAssets:speech-sdk': '/rp/6slp3E-BqFf904Cz6cCWPY1bh9E.br.js' },
-  { 'A:rms:answers:Web:SydneyWelcomeScreenBase':'/rp/tAJNECpUpSdBSxOEHv5jZ0nF93I.br.js' },
-  { 'A:rms:answers:Web:SydneyWelcomeScreen':'/rp/H-Z1_dXpX8YW6Di4p6iwWmTVvMs.br.js' },
-  { 'A:rms:answers:Web:SydneyFullScreenConv': '/rp/2BwrAKR2l9HZV6BTZonHj9yRnZ4.br.js' },
+  { 'A:rms:answers:Web:SydneyWelcomeScreenBase':'/rp/9PCcKuiB-PHZP2Z6JcHqceFYSkU.br.js' },
+  { 'A:rms:answers:Web:SydneyWelcomeScreen':'/rp/MT9w5YvjxYHdcyXSS0tDquzGw2Y.br.js' },
+  { 'A:rms:answers:Web:SydneyFullScreenConv': '/rp/Q3RrjxXC2b-ioHF1_DcBul9NlTA.br.js' },
 );
