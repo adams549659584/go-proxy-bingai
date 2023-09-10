38ce9d4233a148b6f260ba07e181a240347aac43
Harry-zklcdc <1269158832@qq.com>
2023-09-10 14:08:24 +0800

    [Fix] 🐛 Reset Func Bug #204

--- frontend/src/components/ChatNav/ChatNav.vue
+++ frontend/src/components/ChatNav/ChatNav.vue
@@ -195,7 +195,7 @@ const resetCache = async () => {
   isShowClearCacheModal.value = false;
   await userStore.resetCache();
   message.success('清理完成');
-  window.location.reload();
+  window.location.href = '/web/';
 };
 
 const saveSetting = () => {
