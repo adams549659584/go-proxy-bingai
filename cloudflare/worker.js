7c786a4693b76d8a2fa3bd93f218cef93a18baaa
Harry-zklcdc <zklcdc@qq.com>
2024-02-16 02:05:06 +0800

    [Fix] 🐛 Bypass Request Error

--- cloudflare/worker.js
+++ cloudflare/worker.js
@@ -245,7 +245,7 @@ const challengeResponseBody = `
 				convId = window.parent.CIB.manager.conversation.id,
 				rid = window.parent.CIB.manager.conversation.messages[0].requestId,
 				iframeid = '{{%s}}',
-				T = window.parent.aesEncrypt(e, window.parent._G.IG);
+				T = await window.parent.aesEncrypt(e, window.parent._G.IG);
 			await fetch('/challenge/verify?IG='+encodeURI(IG)+'&iframeid='+encodeURI(iframeid)+'&convId='+encodeURI(convId)+'&rid='+encodeURI(rid)+'&T='+encodeURI(T), {
 				credentials: 'include',
 			}).then((res) => {
