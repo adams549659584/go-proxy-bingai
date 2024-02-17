9973b5baa03769421f17a144d17f542e4d35ecfc
Harry-zklcdc <zklcdc@qq.com>
2024-02-17 21:25:56 +0800

    [Fix] 🐛 Sysconf Return Struct Error

--- frontend/src/api/sysconf.ts
+++ frontend/src/api/sysconf.ts
@@ -1,11 +1,21 @@
-import type { ApiResult } from './model/ApiResult';
+import type { ApiResultCode, ApiResult } from './model/ApiResult';
 import type { SysConfig } from './model/sysconf/SysConfig';
 
 export async function getSysConfig() {
   const url = '/sysconf?IG='+_G.IG+'&T='+encodeURI(await aesEncrypt(_G.AT, _G.IG));
   return fetch(url, {
     credentials: 'include',
-  }).then((res) => res.json() as unknown as ApiResult<SysConfig>);
+  }).then((res) => {
+    if (res.ok) {
+      return res.json() as unknown as ApiResult<SysConfig>
+    }
+    let resp:ApiResult<SysConfig> = {
+      code: res.status as ApiResultCode,
+      message: '',
+      data: null as unknown as SysConfig,
+    }
+    return resp;
+  });
 }
 
 const sysConfApi = {
